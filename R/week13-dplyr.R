be209990d3bd157a88a2981be13d14267f506eda
evanknep <knepx001@umn.edu>
2024-04-29 11:18:58 -0500

    added the joined week13_tbl and saved the SQL tables as .csvs in data folder

--- R/week13-dplyr.R
+++ R/week13-dplyr.R
@@ -1,6 +1,6 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-library(dplyr)
+library(tidyverse)
 library(RMariaDB)
 
 # Data Import and Cleaning
@@ -12,7 +12,16 @@ conn <- dbConnect(MariaDB(),
                   ssl.ca = "../mysql_hotel_umn_20220728_interm.cer")
 
 dbGetQuery(conn, "SHOW DATABASES;")
-result1 <- dbExecute(conn, "USE cla_tntlab;")
+result1 <- dbExecute(conn, "USE cla_tntlab;") 
 employees_tbl <- dbGetQuery(conn, "SELECT * FROM datascience_employees")
 offices_tbl <- dbGetQuery(conn, "SELECT * FROM datascience_offices")
 testscores_tbl <- dbGetQuery(conn, "SELECT * FROM datascience_testscores")
+
+write_csv(employees_tbl, "../data/employees_tbl.csv")
+write_csv(offices_tbl, "../data/offices_tbl.csv")
+write_csv(testscores_tbl, "../data/testscores_tbl.csv")
+
+week13_tbl <- right_join(employees_tbl, testscores_tbl, by = "employee_id") %>% #right join on testscores to remove employees without a test score
+  left_join(offices_tbl, by = join_by(city == office))
+
+
